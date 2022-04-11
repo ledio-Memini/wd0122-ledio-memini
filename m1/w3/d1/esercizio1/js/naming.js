647b76346e27cba02d0a57f2df4e29be77a1e1b9
Ledio-Memini <lediothegod@gmail.com>
2022-04-11 15:25:05 +0200

    javascript

---
+++ m1/w3/d1/esercizio1/js/naming.js
@@ -0,0 +1,19 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var petPreferito = 'coniglio';
+document.write(petPreferito, '<br>');
+
+
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
